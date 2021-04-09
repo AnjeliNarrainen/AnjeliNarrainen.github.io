b3da0a29292a10a52f9a7526f13c2814d3da9f10
Anjeli <anjeli.narrainen@polytmtl.ca>
2021-04-09 17:42:54 +0400

    Updates

---
+++ precache-manifest.f26810a39d21fd883c8ba386f34fcc83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65080fdb48bdde79d36181de86475e99",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "eaaa4a88e63f2cad3bd8",
+    "url": "/blindtest-spotify/static/css/main.12217c1b.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaaa4a88e63f2cad3bd8",
+    "url": "/blindtest-spotify/static/js/main.9018ad1c.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);

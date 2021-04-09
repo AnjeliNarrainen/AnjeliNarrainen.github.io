075e7cdc246d64396b421e78b9a428033985a870
Anjeli <anjeli.narrainen@polytmtl.ca>
2021-04-09 18:02:26 +0400

    Updates

---
+++ precache-manifest.babdfd728fc891484a40b15fbe0abf1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3809b3d05d4c956f2e02b296589a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ab2bab0c6701c8b7b0",
+    "url": "/static/css/main.12217c1b.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ab2bab0c6701c8b7b0",
+    "url": "/static/js/main.042038d5.chunk.js"
+  },
+  {
+    "revision": "76d92348da833961d0cc",
+    "url": "/static/js/runtime-main.9cd9e70b.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/static/media/logo.6a581294.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.f26810a39d21fd883c8ba386f34fcc83.js"
+  "/precache-manifest.babdfd728fc891484a40b15fbe0abf1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blindtest-spotify/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
